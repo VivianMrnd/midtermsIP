fb73c74507bed6d6b33c8163f1107a71563329be
VivianMrnd <vivian112918miranda@gmail.com>
2019-02-14 11:45:43 +0800

    add more monsters

--- Draw.java
+++ Draw.java
@@ -22,10 +22,14 @@ public class Draw extends JComponent{
 
 	Monster monster1;
 	Monster monster2;
+	Monster monster3;
+	Monster monster4;
 
 	public Draw(){
 		monster1 = new Monster(200, 200);
 		monster2 = new Monster(300, 200);
+		monster3 = new Monster(400, 200);
+		monster4 = new Monster(500, 200);
 
 		try{
 			image = ImageIO.read(resource);
@@ -132,5 +136,7 @@ public class Draw extends JComponent{
 
 		g.drawImage(monster1.image, monster1.xPos, monster1.yPos, this);
 		g.drawImage(monster2.image, monster2.xPos, monster2.yPos, this);
+		g.drawImage(monster3.image, monster3.xPos, monster3.yPos, this);
+		g.drawImage(monster4.image, monster4.xPos, monster4.yPos, this);
 	}
 }
